6492dbe9a09b351fe88644e08e56e6670417b40d
tcostain98 <tomcostain7@me.com>
2021-08-06 11:54:49 +0000

    debugged but icons still not showing on webpage

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,68 +1,68 @@
-var emoji = [
-    "angry",
-    "dizzy",
-    "flushed",
-    "frown",
-    "grimace",
-    "grin-alt",
-    "grin-beam",
-    "grin-beam-sweat",
-    "grin-hearts",
-    "grin-squint",
-    "grin-squint-tears",
-    "grin-stars",
-    "grin-tears",
-    "grin-tongue-squint",
-    "grin-wink",
-    "kiss",
-    "meh",
-    "meh-rolling-eyes",
-    "smile-beam",
-    "surprise",
-    "tired",
-    "smile",
+var brands = [
+"angry",
+"dizzy",
+"flushed",
+"frown",
+"grimace",
+"kiss",
+"meh",
+"surprise",
+"tired",
+"smile",
+"meh-blank",
+"grin-squint",
+"grin-stars",
+"grin-tongue-squint",
+"grin-hearts",
+"laugh-squint",
+"grin-squint-tears",
+"kiss-wink-heart",
+"sad-cry",
+"grin-alt",
 ];
 
 const startGame = () => {
-    const gameContainer = document.getElementById('gameContainer');
-    gameContainer.innerHTML='';
-    var scoreDiv = document.getElementById('level').value;
-    var score = 0, difficulty;
-    
-    if (level == 'easy') difficulty = 4;
-    else if (level == 'medium') difficulty = 6;
-    else if (level == 'hard') difficulty = 8;
-    else if (level == 'expert') difficulty = 10;
+gameContainer.innerHTML ='';
+const gameContainer = document.getElementById('gameContainer');
+var scoreDiv = document.getElementById('score');
+const level = document.getElementById('level').value;
+var score = 0, difficulty;
 
-    var emoji2 = JSON.parse(JSON.stringify(emoji));
-    var selectedEmoji = [];
+if (level == 'easy') difficulty = 4;
+else if (level == 'medium') difficulty = 6;
+else if (level == 'hard') difficulty = 8;
+else if (level == 'expert') difficulty = 10;
 
+var brands2 = JSON.parse(JSON.stringify(brands));
+var selectedBrand = [];
 
-    for(var i = (difficulty*difficulty)/2;i>0;--i){
-        var randomEmoji = emoji2.splice(Math.floor(Math.random()*emoji2.length, 1));
+for (var i = (difficulty*difficulty)/2;i>0;--i) {
+var randomBrand = brands2.splice(Math.floor(Math.random()*brands2.length),1);
 
-        selectedEmoji.push(randomEmoji);
-        selectedEmoji.push(randomEmoji);
-        
-    }
+selectedBrand.push(randomBrand);
+selectedBrand.push(randomBrand);
 
-    for(var i =difficulty;i>0;--i){
-        var row = document.createElement('div');
-        for(var i = difficulty;j>0;--i){
-            var col = document.createElement('i');
-            col.classList.add('fab');
-            col.classList.add('fa-'+selectedEmoji.splice(Math.floor(Math.random()*selectedEmoji.length, 1)))
-
-            row.append(col);
-        }
-
-        gameContainer.append(row);
-        }
-
-
-    
-    return false;
 }
+
+for (var i = difficulty;i>0;--i){
+var row = document.createElement('div')
+for(var j = difficulty;j>0;--j){
+var col = document.createElement('i');
+col.classList.add('fab');
+col.classList.add('fa-'+selectedBrand.splice(Math.floor(Math.random()*selectedBrand.length),1));
+
+row.append(col);
+
+}
+gameContainer.append(row);
+
+}
+
+return false;
+
+
+}
+
 
 // <i class='fab fa-smile'/>
 
