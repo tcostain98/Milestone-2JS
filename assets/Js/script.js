ba5ec07b648e686594be8a4bc2dbebeff89a1bac
tcostain98 <tomcostain7@me.com>
2021-08-09 20:04:48 +0000

    added working countdown

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -51,6 +51,47 @@ var brands=[
 "twitch",
 ];
 
+function startTimer(duration, display) {
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+    function timer() {
+        // get the number of seconds that have elapsed since 
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds; 
+
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
+        }
+    };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
 const startGame = () => {
 const gameContainer = document.getElementById('gameContainer');
 gameContainer.innerHTML = '';
@@ -111,21 +152,8 @@ if(score == (difficulty*difficulty)/2) {
 
 })
 
-{ const startingMinutes = 5;
- let time = startingMinutes *60
 
- setInterval(updateCountdown,1000);
 
- const countdownEL = document.getElementById('countdown');
-const minutes = Math.floor(time / 60);
-let seconds = time % 60;
-
-seconds = seconds < 10 ? '0' + seconds : seconds;
-
-countdownEL.innerHTML = `${minutes}:${seconds}`;
- time--;
-
-}
 
 
 row.append(col);
